4aa795fb173255f91ac970c34ef051c5f73d9d18
Zedjones <dojoman19@gmail.com>
2020-10-21 21:12:12 -0400

    Refactored some of the functionality and switched to dotenv

--- start_bot.sh
+++ start_bot.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 ts3_bot.py
+poetry run python3 ts3_bot.py
--- ts3_bot.py
+++ ts3_bot.py
@@ -1,8 +1,14 @@
 import json
+import os
 import threading
+from typing import List
 
+from dotenv import load_dotenv
 from matrix_bot_api.matrix_bot_api import MatrixBotAPI
 from matrix_bot_api.mregex_handler import MRegexHandler
+from matrix_client.room import Room
+
+import preflyt
 import ts3
 
 CONFIG_FILE = "bot_cfg.json"
@@ -10,21 +16,33 @@ URI = None
 
 
 def main():
+    load_dotenv()
     global URI
 
-    config = json.loads(open("bot_cfg.json", "r").read())
-    user, password, server = (
-        config["matrix_username"],
-        config["matrix_pass"],
-        config["matrix_server"],
+    ok, _ = preflyt.check(
+        [
+            {"checker": "env", "name": "MATRIX_USERNAME"},
+            {"checker": "env", "name": "MATRIX_PASS"},
+            {"checker": "env", "name": "MATRIX_SERVER"},
+            {"checker": "env", "name": "TS_URI"},
+            {"checker": "env", "name": "EVENT_ROOMS"},
+        ]
     )
-    URI = config["ts_uri"]
-    event_rooms = config["event_rooms"]
+
+    if not ok:
+        print("Not all required config options are specified")
+        return
+
+    user, password, server = (
+        os.getenv("MATRIX_USERNAME"),
+        os.getenv("MATRIX_PASS"),
+        os.getenv("MATRIX_SERVER"),
+    )
+    URI = os.getenv("TS_URI")
+    event_rooms = os.getenv("EVENT_ROOMS")
 
     matrix_bot = setup_matrix_bot(user, password, server)
-    ts3_thread = threading.Thread(
-        target=check_join_and_leave, args=(matrix_bot, event_rooms)
-    )
+    ts3_thread = threading.Thread(target=check_join_and_leave, args=(matrix_bot, event_rooms))
 
     ts3_thread.start()
     ts3_thread.join()
@@ -41,24 +59,21 @@ def setup_matrix_bot(username, password, server):
     return bot
 
 
-def show_online_clients(room, _):
+def show_online_clients(room: Room, _):
     with ts3.query.TS3ServerConnection(URI) as ts3conn:
         ts3conn.exec_("use", sid=1)
 
         clients = []
 
         for client in ts3conn.exec_("clientlist"):
-            if (
-                client.get("client_nickname") is not None
-                and client.get("client_type") == "0"
-            ):
+            if client.get("client_nickname") is not None and client.get("client_type") == "0":
                 print(client)
                 clients.append(client.get("client_nickname"))
 
         room.send_text("Users online: " + ", ".join(clients))
 
 
-def check_join_and_leave(bot: MatrixBotAPI, send_rooms):
+def check_join_and_leave(bot: MatrixBotAPI, send_rooms: List[str]):
     """
     :param bot: is a bot
     """
@@ -66,7 +81,7 @@ def check_join_and_leave(bot: MatrixBotAPI, send_rooms):
     # maps clid to client_nickname
     online_clients = {}
 
-    room_objs = []
+    room_objs: List[Room] = []
 
     for send_room in send_rooms:
         room_objs.append(bot.client.rooms.get(send_room))
@@ -85,30 +100,26 @@ def check_join_and_leave(bot: MatrixBotAPI, send_rooms):
             except (ts3.query.TS3TimeoutError, ts3.query.TS3QueryError):
                 pass
             else:
+                room_text = ""
                 # Greet new clients.
-                if event[0]["reasonid"] == "0":
-                    if event[0]["client_type"] == "0":
-                        for room in room_objs:
-                            room.send_text(f"{event[0]['client_nickname']} connected")
-                        online_clients[event[0]["clid"]] = event[0]["client_nickname"]
+                if event[0]["reasonid"] == "0" and event[0]["client_type"] == "0":
+                    room_text = f"{event[0]['client_nickname']} connected"
+                    online_clients[event[0]["clid"]] = event[0]["client_nickname"]
                 elif event[0]["reasonid"] == "8":
+                    if event[0]["clid"] in online_clients:
+                        room_text = f"{online_clients.get(event[0]['clid'])} disconnected"
+                        online_clients.pop(event[0]["clid"])
+                    else:
+                        room_text = "Somebody disconnected"
+                elif event[0]["reasonid"] == "4":
+                    if event[0]["clid"] in online_clients:
+                        room_text = f"{online_clients.get(event[0]['clid'])} was kicked"
+                        online_clients.pop(event[0]["clid"])
+                    else:
+                        room_text = "Somebody was kicked"
+                if room_text != "":
                     for room in room_objs:
-                        if event[0]["clid"] in online_clients:
-                            room.send_text(
-                                f"{online_clients.get(event[0]['clid'])} disconnected"
-                            )
-                            online_clients.pop(event[0]["clid"])
-                        else:
-                            room.send_text("Somebody disconnected")
-                elif event[0]["reasonid"] == "4" or event[0]["reasonid"] == "4":
-                    for room in room_objs:
-                        if event[0]["clid"] in online_clients:
-                            room.send_text(
-                                f"{online_clients.get(event[0]['clid'])} was kicked"
-                            )
-                            online_clients.pop(event[0]["clid"])
-                        else:
-                            room.send_text("Somebody was kicked")
+                        room.send_text(room_text)
 
 
 if __name__ == "__main__":
